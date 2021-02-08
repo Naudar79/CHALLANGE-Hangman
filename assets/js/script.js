ef018befa56d0a3fe2b43790025c9589c595008e
Naudar79 <naudar79@gmail.com>
2021-02-08 18:16:54 +0100

    Last commit for today

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+(() => {
+    let alpha = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+    let list = [cheval, rouler, moderne, neige, orange, ordinateur, potiron, ours, pays, pizza, classe, formation, hamburger, sauce];
+    let gallery = ["./assets/img/P1.png", "./assets/img/P2.png", "./assets/img/P3.png", "./assets/img/P4.png", "./assets/img/P5.png", "./assets/img/P6.png", "./assets/img/P7.png", ];
+
+    document.getElementById("nouv").addEventListener("click", () => {
+
+    });
+})();
