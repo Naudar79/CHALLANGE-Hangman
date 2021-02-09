44f2954dd8af3c86af67057c5b267c6229b6bb1f
Naudar79 <naudar79@gmail.com>
2021-02-09 11:04:04 +0100

    work at html, css, js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,27 @@
     let alpha = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
     let list = [cheval, rouler, moderne, neige, orange, ordinateur, potiron, ours, pays, pizza, classe, formation, hamburger, sauce];
     let gallery = ["./assets/img/P1.png", "./assets/img/P2.png", "./assets/img/P3.png", "./assets/img/P4.png", "./assets/img/P5.png", "./assets/img/P6.png", "./assets/img/P7.png", ];
-
-    // démarrer une nouvelle partie au clique du bouton
+    let array = []
+    let tiret = "_"
+        // démarrer une nouvelle partie au clique du bouton
     document.getElementById("nouv").addEventListener("click", () => {
         /* choisir un mot au hasard dans "list", le mettre dans une nouvelle table 
         en faisant un split du mot et ainsi détecter sa longueur*/
-
+        function selectWord() {
+            function returnIndex(index) {
+                return Math.floor(Math.random() * Math.floor(index));
+            }
+            return alpha[returnIndex(alpha.length)];
+        }
         /*afficher autant de "_" que de nombres de lettre du mot*/
 
         /*capter l'entée au clavier de l'utilisateur*/
-        /*Si la lettre entrée est bonne, l'afficher autant de fois qu'elle est présente 
-        dans le mot et au bon endroit*/
-        /*Si la lettre n'est pas présente, l'afficher dans la liste des mauvaises 
-        lettres, ajouter +1 au compteur d'essais (7 fois max) et passer à l'image suivante*/
-        /*Si l'utilisateur ne trouve pas le mot, lui dire qu'il a perdu et arrêter le jeu*/
+        let choix =
+            /*Si la lettre entrée est bonne, l'afficher autant de fois qu'elle est présente 
+            dans le mot et au bon endroit*/
+            /*Si la lettre n'est pas présente, l'afficher dans la liste des mauvaises 
+            lettres, ajouter +1 au compteur d'essais (7 fois max) et passer à l'image suivante*/
+
+            /*Si l'utilisateur ne trouve pas le mot, lui dire qu'il a perdu et arrêter le jeu*/
     });
 })();
